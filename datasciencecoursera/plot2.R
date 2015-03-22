c02ff9f050f65fdea5e020964fbe89629116edf2
drju01 <akisiel@onet.eu>
2015-03-22 23:29:09 +0100

    submission

--- datasciencecoursera/plot2.R
+++
@@ -1,9 +0,0 @@
-library(data.table)
-setwd("E:/Coursera - Exploratory Analysis/exdata-data-household_power_consumption")
-DT<-fread("household_power_consumption.txt")
-DT2<-subset(DT, as.Date(Date,"%d/%m/%Y") >= '2007-02-01' & as.Date(Date,"%d/%m/%Y") <= '2007-02-02')
-
-with(DT2,plot(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"),Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)"))
-
-dev.copy(png,file="plot2.png")
-dev.off()
